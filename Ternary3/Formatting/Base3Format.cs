ed1e0ad5cfb0cb2beb8ac952467f8abd2a27df2e
Bart Kemps <b.kemps@rdc.nl>
2022-05-22 03:20:44 +0200

    Some unit tests

--- Ternary3/Formatting/Base3Format.cs
+++ Ternary3/Formatting/Base3Format.cs
@@ -6,7 +6,7 @@ public class Base3Format : IBase3Format
     private Base3Format(string digits) => this.digits = digits;
     public static Base3Format LetterFormat { get; } = new Base3Format("MDU?");
     public static Base3Format SignFormat { get; } = new Base3Format("0-+?");
-    public static Base3Format NumberFormat { get; } = new Base3Format("0i1?");
+    public static Base3Format NumberFormat { get; } = new Base3Format("0T1?");
     string IBase3Format.Digits => digits;
     public static IBase3Format Create(char down, char middle, char up) => new Base3Format(new string(new[] { middle, down, up, '?' }));
     public static IBase3Format Create(string digits)
---
+++ Ternary3Tests/TernaryInt/TernaryInt27Tests.cs
@@ -0,0 +1,49 @@
+﻿namespace TernaryTests.TernaryInt;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ternary.Formatting;
+
+public class TernaryInt27Tests
+{
+    [Fact]
+    public void MinValue_ReturnsValue()
+    {
+        var value = (TernaryInt27)TernaryInt27.MinValue;
+        value.ToString().Should().Be("DDDDDDDDDDDDDDDDDDDDDDDDDDD");
+    }
+
+    [Fact]
+    public void MaxValue_ReturnsValue()
+    {
+        var value = (TernaryInt27)TernaryInt27.MaxValue;
+        value.ToString().Should().Be("UUUUUUUUUUUUUUUUUUUUUUUUUUU");
+    }
+
+    [Fact]
+    public void ToString_FormatsValue()
+    {
+        var value = (TernaryInt27)2;
+        value.ToString(Base3Format.SignFormat, 0).Should().Be("+-");
+        value.ToString(Base3Format.NumberFormat, 6).Should().Be("00001T");
+        value.ToString(Base3Format.LetterFormat).Should().Be("MMMMMMMMMMMMMMUD");
+    }
+
+    [Theory]
+    [InlineData(TernaryInt27.MinValue)]
+    [InlineData(TernaryInt27.MaxValue)]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(1)]
+    public void Parse_ParsesValue(long value)
+    {
+        var expected = (TernaryInt27)value;
+        var txt = expected.ToString();
+        var actual = TernaryInt27.Parse(txt);
+        expected.Should().Be(actual);
+    }
+}
+
